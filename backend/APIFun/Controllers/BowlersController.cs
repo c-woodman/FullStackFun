fc8c9c8fc149b1f249470056c18e2034c35c1d31
c-woodman <cwoodma1@byu.edu>
2024-03-15 10:38:07 -0600

    test

---
+++ backend/APIFun/Controllers/BowlersController.cs
@@ -0,0 +1,40 @@
+﻿using APIFun.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIFun.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BowlersController : ControllerBase
+    {
+
+        private IBowlerRepository _bowlerRepository;
+        public BowlersController(IBowlerRepository temp) {
+            _bowlerRepository = temp;
+        }
+
+        [HttpGet]
+        public IEnumerable<Object> Get()
+        {
+            var joinedData = (from bowler in _bowlerRepository.Bowlers
+                              join team in _bowlerRepository.Teams on bowler.TeamID equals team.TeamID
+                              where team.TeamName == "Marlins" | team.TeamName == "Sharks" 
+                              select new
+                              {
+                                  BowlerID = bowler.BowlerID,
+                                  BowlerFirstName = bowler.BowlerFirstName,
+                                  BowlerMiddleInit = bowler.BowlerMiddleInit, 
+                                  BowlerLastName = bowler.BowlerLastName,
+                                  TeamName = team.TeamName,
+                                  BowlerAddress = bowler.BowlerAddress,
+                                  BowlerCity = bowler.BowlerCity,
+                                  BowlerState = bowler.BowlerState,
+                                  BowlerZip = bowler.BowlerZip,
+                                  BowlerPhoneNumber = bowler.BowlerPhoneNumber
+                              }).ToArray();
+
+            return joinedData;
+        }
+    }
+}
---
+++ backend/APIFun/Data/EFBowlerRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using SQLitePCL;
+
+namespace APIFun.Data
+{
+    public class EFBowlerRepository : IBowlerRepository
+    {
+        private BowlerContext _context;
+        public EFBowlerRepository(BowlerContext temp) {
+            _context = temp;
+        }
+        public IEnumerable<Bowler> Bowlers => _context.Bowlers;
+        public IEnumerable<Team> Teams => _context.Teams;
+    }
+}
