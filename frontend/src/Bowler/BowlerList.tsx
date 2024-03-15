328071a4dc80f60a023db59eb5c99fab63e75bc7
c-woodman <cwoodma1@byu.edu>
2024-03-15 10:36:47 -0600

    push all changes

---
+++ frontend/src/Bowler/BowlerList.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Bowler } from '../types/Bowler';
+
+function BowlerList() {
+  const [bowlerData, setBowlerData] = useState<Bowler[]>([]);
+
+  useEffect(() => {
+    const fetchBowlerData = async () => {
+      const rsp = await fetch('http://localhost:5254/Bowlers');
+      const f = await rsp.json();
+      setBowlerData(f);
+    };
+    fetchBowlerData();
+  }, []);
+
+  return (
+    <>
+      <div className="row">
+        <h4 className="text-center">Bowlers League</h4>
+      </div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Bowler Name</th>
+            <th>Team Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bowlerData.map((f) => (
+            <tr key={f.bowlerId}>
+              <td>{`${f.bowlerFirstName} ${f.bowlerMiddleInit ? f.bowlerMiddleInit + '. ' : ''}${f.bowlerLastName}`}</td>
+              <td>{f.teamName}</td>
+              <td>{f.bowlerAddress}</td>
+              <td>{f.bowlerCity}</td>
+              <td>{f.bowlerState}</td>
+              <td>{f.bowlerZip}</td>
+              <td>{f.bowlerPhoneNumber}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+export default BowlerList;
